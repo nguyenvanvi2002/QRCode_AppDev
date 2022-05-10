14fd09855838bf00ce95f2a04dc9bf39bf8726dc
roglap <nguyenvanvi0812@gmail.com>
2022-05-10 21:39:17 +0700

    swipe views bottom menu

--- app/src/main/java/com/example/qrcode_appdev/MainActivity.java
+++ app/src/main/java/com/example/qrcode_appdev/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.qrcode_appdev;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
@@ -16,6 +19,7 @@ import java.util.Set;
 public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView ;
+    private ViewPager mViewPager;
     QrScanFragment scanFragment = new QrScanFragment();
     CreateFragment createFragment = new CreateFragment();
     HistoryFragment historyFragment = new HistoryFragment();
@@ -27,27 +31,66 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         bottomNavigationView = findViewById(R.id.bottom_navigation);
+        mViewPager = findViewById(R.id.view_pager);
+
+        setupViewPager();
+
         getSupportFragmentManager().beginTransaction().replace(R.id.view_pager, scanFragment).commit(); //set the begin fragment to scan
-        bottomNavigationView.setSelectedItemId(R.id.qrscan); //set the begin tab to scan
+        bottomNavigationView.setSelectedItemId(R.id.action_scan); //set the begin tab to scan
 
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()){
-                    case R.id.qrscan:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.view_pager, scanFragment).commit();
+                    case R.id.action_scan:
+                        mViewPager.setCurrentItem(0);
                         return true;
-                    case R.id.create:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.view_pager, createFragment).commit();
+                    case R.id.action_create:
+                        mViewPager.setCurrentItem(1);
                         return true;
-                    case R.id.history:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.view_pager, historyFragment).commit();
+                    case R.id.action_history:
+                        mViewPager.setCurrentItem(2);
                         return true;
-                    case R.id.setting:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.view_pager, settingFragment).commit();
+                    case R.id.action_setting:
+                        mViewPager.setCurrentItem(3);
                         return true;
                 }
                 return false;
+            }
+        });
+    }
+
+    private void setupViewPager(){
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        mViewPager.setAdapter(viewPagerAdapter);
+
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                switch (position){
+                    case 0:
+                        bottomNavigationView.getMenu().findItem(R.id.action_scan).setChecked(true);
+                        break;
+                    case 1:
+                        bottomNavigationView.getMenu().findItem(R.id.action_create).setChecked(true);
+                        break;
+                    case 2:
+                        bottomNavigationView.getMenu().findItem(R.id.action_history).setChecked(true);
+                        break;
+                    case 3:
+                        bottomNavigationView.getMenu().findItem(R.id.action_setting).setChecked(true);
+                        break;
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
             }
         });
     }
---
+++ app/src/main/java/com/example/qrcode_appdev/ViewPagerAdapter.java
@@ -0,0 +1,34 @@
+package com.example.qrcode_appdev;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new QrScanFragment();
+            case 1:
+                return new CreateFragment();
+            case 2:
+                return new HistoryFragment();
+            case 3:
+                return new SettingFragment();
+            default: return new QrScanFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+}
